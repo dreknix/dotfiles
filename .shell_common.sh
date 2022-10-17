6d4fa1a5c6882b79caa7001a325dd0a285ea4cde
dreknix <dreknix@proton.me>
2022-10-17 12:54:11 +0200

    Switch to source of base16 helper

--- .shell_common.sh
+++ .shell_common.sh
@@ -10,7 +10,7 @@ umask 0022
 BASE16_SHELL="$HOME/.config/base16-shell/"
 [ -n "$PS1" ] && \
   [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-    eval "$("$BASE16_SHELL/profile_helper.sh")"
+    . "$BASE16_SHELL/profile_helper.sh"
 
 # activate same graphical theme in Qt applications
 export QT_STYLE_OVERRIDE=kvantum
