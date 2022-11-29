6a9c82c35341438fea5cc288b327d823edee8ff6
dreknix <dreknix@proton.me>
2022-11-29 12:50:33 +0100

    Add the IP address to tmux status bar

---
+++ bin/tmux-get-ip.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+__route_str="$(ip route get 8.8.8.8 | head -1)";
+if echo "${__route_str}" | /usr/bin/grep -q " via "
+then
+  __ip=$(echo "${__route_str}" | cut -d' ' -f7)
+else
+  __ip=$(echo "${__route_str}" | cut -d' ' -f5)
+fi
+
+__if=$(ip address show to "${__ip}" | head -1 | cut -f2 -d: | tr -d ' ' | cut -c1-2)
+
+case "${__if}" in
+  en)
+    echo -n " "
+    ;;
+  wl)
+    echo -n "直"
+    ;;
+  vp)
+    echo -n " "
+    ;;
+  *)
+    echo -n " ${__if} "
+    ;;
+esac
+
+
+echo "${__ip}"
