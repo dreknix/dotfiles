b3c04aa99ef7003766cf01c63aa37d7841489ae9
dreknix <dreknix@proton.me>
2022-11-30 12:48:35 +0100

    Add better session display in status bar and left- and right-marker in window list

---
+++ bin/tmux-get-session.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# check for SSH 
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_CONNECTION" ] || [ -n "$SSH_TTY" ]; then
+  echo -n " "
+fi
+
+# check for Windows
+if grep -qi microsoft /proc/version
+then
+  echo -n " "
+fi
+
+case "$(grep -e '^ID=' /etc/os-release | cut -f 2 -d '=')" in
+  ubuntu)
+    case "$(grep -e '^NAME=' /etc/os-release | cut -f 2 -d '=')" in
+      Pop!_OS)
+        echo -n " "
+        ;;
+      *)
+        echo -n " "
+        ;;
+    esac
+    ;;
+  debian)
+    echo -n " "
+    ;;
+  raspbian)
+    echo -n " "
+    ;;
+  rhel)
+    echo -n " "
+    ;;
+  *)
+    echo -n " "
+    ;;
+esac
+
+echo " $1($(tmux list-clients -t "$1" | wc -l))"
