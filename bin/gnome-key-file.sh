36f1a7d6907c03b3bdadb2eede526b442e641e84
dreknix <dreknix@proton.me>
2024-04-16 12:07:23 +0200

    Switch to locate

--- bin/gnome-key-file.sh
+++ bin/gnome-key-file.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 #
-# sudo apt install rofi mlocate
+# sudo apt install rofi locate
 #
 
 # theme (font/color) in `.config/rofi/config.rasi`
 
 xdg-open "$(locate -e -i --regex "^/home/$USER/[^\.]+" | rofi -dmenu -i)" > /dev/null 2>&1
-
