03c842d1b6bf5a74597de9e9ec901e60d193b6dc
dreknix <dreknix@proton.me>
2024-10-07 16:07:09 +0200

    Update to new NvChad version and fix status line

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -12,28 +12,19 @@ M.base46 = {
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+}
 
+M.ui = {
   statusline = {
     theme = 'default',  -- default/vscode/vscode_colored/minimal
-    separator_style = 'block',  -- default/round/block/arrow
+    separator_style = 'round',  -- default/round/block/arrow
 
-    overriden_modules = function(modules)
-      modules[10] = (function()
-        local left_sep = '%#St_pos_sep#' .. '█' .. '%#St_pos_icon#' .. ' '
-        local current_column = vim.fn.col '.'
-        local current_line = vim.fn.line '.'
-        local total_line = vim.fn.line '$'
-        local percentage = math.modf((current_line / total_line) * 100)
-        local text = string.format('%2d', percentage) .. '%%'
-
-        text = (current_line == 1 and 'Top') or text
-        text = (current_line == total_line and 'Bot') or text
-
-        text = string.format('%3s %-3d', text, current_column)
-
-        return left_sep .. '%#St_pos_text#' .. ' ' .. text .. ' '
-      end)()
-    end,
+    modules = {
+      -- see: ~/.local/share/nvim/lazy/ui/lua/nvchad/stl
+      cursor = function()
+        return "%#St_pos_sep#%#St_pos_icon# %#St_pos_text# %04l:%03c "
+      end,
+    },
   },
 
 }
