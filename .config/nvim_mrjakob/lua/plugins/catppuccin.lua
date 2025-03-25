eaf61a2f42a66dbc464d0799a87badd8c2be1ccf
dreknix <dreknix@proton.me>
2025-03-25 20:17:15 +0100

    feat: switch from kanagawa to catppuccin

---
+++ .config/nvim_mrjakob/lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+-- https://github.com/catppuccin/nvim
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function ()
+    require("catppuccin").setup({
+      flavor = "mocha"
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end,
+}
