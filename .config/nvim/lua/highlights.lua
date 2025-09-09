3ff5eb51ec928bcfa71064c9846252d7b24eed58
dreknix <dreknix@proton.me>
2025-09-09 08:42:56 +0200

    fix: better color for comments

--- .config/nvim/lua/highlights.lua
+++ .config/nvim/lua/highlights.lua
@@ -10,8 +10,12 @@ local M = {}
 M.override = {
   Comment = {
     italic = true,
-    bold = true,
+    -- bold = true,
+    -- fg = "#7c7f93",  -- overlay2
+    -- fg = "#8c8fa1",  -- overlay1
+    fg = "#9ca0b0",  -- overlay0
   },
+  ["@comment"] = { link = "Comment" },
 }
 
 M.add = {
