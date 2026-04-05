5b78fe0ae03c4f08fc5919e24dd85a08641f6b65
dreknix <dreknix@proton.me>
2026-04-05 21:07:46 +0200

    chore: add delete and copy to clipboard

--- .config/tmux/scripts/choose-buffer.sh
+++ .config/tmux/scripts/choose-buffer.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+#
+# Enter - paste the selected buffer into current pane
+# Ctrl-d - delete the selected buffer
+# Ctrl-v - copy the selected buffer to clipboard
+#
+
 tmux list-buffer -F "#{buffer_name}" | \
   fzf \
     --tmux 80%,80% --border-label ' Choose TMUX Buffer ' \
+    --header '^d - delete | ^v - copy' \
+    --bind 'ctrl-d:execute(tmux delete-buffer -b {})+reload(tmux list-buffer -F "#{buffer_name}")' \
+    --bind 'ctrl-v:execute(tmux show-buffer -b {} | dreknix_clipboard.sh)+abort' \
+    --preview-window 'right:70%' \
     --preview "tmux show-buffer -b {}" | \
   xargs --no-run-if-empty tmux paste-buffer -r -b
