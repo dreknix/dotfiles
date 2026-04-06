39214b11c352cd90414c4d8701c0be27ea64e0c2
dreknix <dreknix@proton.me>
2026-04-06 19:40:55 +0200

    chore: better fzf handling of tmux choose-buffer

--- .config/tmux/scripts/choose-buffer.sh
+++ .config/tmux/scripts/choose-buffer.sh
@@ -6,16 +6,24 @@
 # Ctrl-v - copy the selected buffer to clipboard
 #
 
-tmux list-buffer -F "#{buffer_name}" | \
+# Multi column support in fzf:
+# --delimiter    - specify the column delimiter
+# --with-nth     - string for the list entry, e.g., "{2} ({1}): {3..}"
+# --accept-nth   - which column is used after selection
+# In preview or bind {1} can select a specific column.
+
+tmux list-buffer -F "#{buffer_name};#{=50:buffer_sample}" | \
   fzf \
     --tmux 80%,80% \
+    --no-multi \
+    --with-nth "{2..}" --delimiter ";" --accept-nth 1 \
     --border-label ' Choose TMUX Buffer ' \
     --preview-label ' Buffer Content ' \
     --header-label ' Keybindings ' \
     --list-label ' Buffers ' \
     --header '^d - delete | ^v - copy' \
-    --bind 'ctrl-d:execute(tmux delete-buffer -b {})+reload(tmux list-buffer -F "#{buffer_name}")' \
-    --bind 'ctrl-v:execute(tmux show-buffer -b {} | dreknix_clipboard.sh)+abort' \
+    --bind 'ctrl-d:execute(tmux delete-buffer -b {1})+reload(tmux list-buffer -F "#{buffer_name};#{=50:buffer_sample}")' \
+    --bind 'ctrl-v:execute(tmux show-buffer -b {1} | dreknix_clipboard.sh)+abort' \
     --preview-window 'right:70%' \
-    --preview "tmux show-buffer -b {}" | \
+    --preview "echo -e '{1}\n'; tmux show-buffer -b {1}" | \
   xargs --no-run-if-empty tmux paste-buffer -r -b
