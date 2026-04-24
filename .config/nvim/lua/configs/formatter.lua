62027c4086f72edc34f46d24fd0824bfecf6c0d4
dreknix <dreknix@proton.me>
2026-04-24 22:15:15 +0200

    feat: switch from black to ruff for formatting

--- .config/nvim/lua/configs/formatter.lua
+++ .config/nvim/lua/configs/formatter.lua
@@ -6,8 +6,10 @@ local M = {
       function()
         local util = require("formatter.util")
         return {
-          exe = "black",
-          args = { "-q", "--fast", "--line-length=130", "--stdin-filename", util.escape_path(util.get_current_buffer_file_name()), "-" },
+          -- exe = "black",
+          -- args = { "-q", "--fast", "--stdin-filename", util.escape_path(util.get_current_buffer_file_name()), "-" },
+          exe = "ruff",
+          args = { "format", "-q", "--stdin-filename", util.escape_path(util.get_current_buffer_file_name()), "-" },
           stdin = true,
         }
       end
