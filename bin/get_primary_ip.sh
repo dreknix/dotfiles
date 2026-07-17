d836f862e05cba015999d42e64536dfe4de770ac
dreknix <dreknix@proton.me>
2026-07-17 17:29:32 +0200

    fix: ignore IPv6

--- bin/get_primary_ip.sh
+++ bin/get_primary_ip.sh
@@ -3,7 +3,7 @@
 case "$(uname -s)" in
   Darwin*)
     interface="$(route -n get 1.1 2> /dev/null | awk '/interface: /{print $2}')"
-    ifconfig "${interface}" | awk '/inet/{print $2}'
+    ifconfig "${interface}" | awk '/inet /{print $2}'
     ;;
   *)
     route_str="$(ip route get 1.1 2> /dev/null | head -1)"
